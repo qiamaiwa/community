1ea1762a3cce4ea8d6a125b2d846dd81ec26e801
qiamaiwa <32133620+qiamaiwa@users.noreply.github.com>
2021-12-22 21:24:36 +0800

    first commit

---
+++ src/test/java/llq557/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package llq557.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
